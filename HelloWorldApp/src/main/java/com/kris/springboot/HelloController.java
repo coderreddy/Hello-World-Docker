3becbf0dc5dbd5c5f0af3aefadd782d9cd7bbc6d
krishnakanthmedipally <krishnakanthmedipally@172.20.20.20>
2018-03-31 18:52:38 -0400

    Initial Commit

---
+++ HelloWorldApp/src/main/java/com/kris/springboot/HelloController.java
@@ -0,0 +1,34 @@
+package com.kris.springboot;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/greet")
+	public String getName(String name)
+	{
+		return "Hello World This is "+name;
+	}
+	
+	@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+	}
+}
